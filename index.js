2a593e6f42bc91357e00e810ae88d12ec261538a
Elaina <elainaseki@gmail.com>
2018-12-10 22:53:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var output;
+  for(var i=0; i<lis.length; i++){
+    output=lis[i].innerHTML =parseInt(lis[i].innerHTML)+n ;
+  }
+  return output;
+}
+
+function deepestChild(){
+ var node = document.getElementById('grand-node');
+var nextNode = node.children[0];
+while(nextNode){
+  node = nextNode;
+  console.log(node.children[0]);
+ }
+}
 
