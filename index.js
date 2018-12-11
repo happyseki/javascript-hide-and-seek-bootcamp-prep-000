86af7ad463fb1f18a9b58c76bdb775e56522ca77
Elaina <elainaseki@gmail.com>
2018-12-11 01:59:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var output;
+  for(var i=0; i<lis.length; i++){
+    output=lis[i].innerHTML =parseInt(lis[i].innerHTML)+n ;
+  }
+  return output;
+}
+
+function deepestChild(){
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var output;
+  for(let i=0;i<lis.length-1;i++) {
+    output=lis[i].querySelector("div");
+  }
+return output;
+}
 
