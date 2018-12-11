1f784c80c2bc714b05328d14fb324dab8711c6fc
Elaina <elainaseki@gmail.com>
2018-12-11 01:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var output;
+  for(var i=0; i<lis.length; i++){
+    output=lis[i].innerHTML =parseInt(lis[i].innerHTML)+n ;
+  }
+  return output;
+}
+
+function deepestChild(){
+var node = document.getElementById('grand-node').querySelector('div');
+var nextNode = node.children[0];
+while(nextNode){
+  node = nextNode;
+ return node.children[0];
+ }
+}
 
